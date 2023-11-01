c9730a44d416edf327efab4d58d4ab9191657c2f
Carlos Gómez <122065980+CarlosGomez-99@users.noreply.github.com>
2023-11-01 23:57:04 +0000

    Game incomplete

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+#
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+# import the random library
+import random
+# create a list of play options
+play_options = ["rock", "paper", "scissors"]
+# assign a random play to the computer
+computer = random.choice(play_options)
+# set player to False
+player = False
+# create a while loop to check the player input
+while player == False:
+    # set player to True
+    player = input("rock, paper, scissors?")
+    # check for a tie
+    if player == computer:
+        print("Tie!")
+    # check for rock
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    # check for paper
+    elif player == "paper":
+        if computer == "scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    # check for scissors
+    elif player == "scissors":
+        if computer == "rock":
+            print("You lose!", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+    # player was set to True, but we want it to be False so the loop continues
+    player = False
+    computer = random.choice(play_options)
